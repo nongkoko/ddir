7d679e59cb7ab56de1228beec22abf219b4b96f1
tukangPES <afryanto.livan@outlook.com>
2025-09-02 11:05:04 +0700

    add jomiuns find first untuk limitting result

--- myCore.cs
+++ myCore.cs
@@ -1,37 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using jomiunsExtensions;
+using Vanara.PInvoke;
 
 internal class myCore(string[] theArgs) : core_ddir.theCoreLogic(theArgs)
 {
-    public override IEnumerable<string>? GetAllThePathFileName(int theLimit, string? parFormat, bool fileNameOnly, string theDir, string? searchPattern, SearchOption searchOption)
+    public override IEnumerable<string>? GetAllThePathFileName(int? theLimit, string? parFormat, bool fileNameOnly, string theDir, string? searchPattern, SearchOption searchOption)
     {
-        var theDirInfo = new DirectoryInfo(theDir);
-        var theFiles = theDirInfo.EnumerateFiles(searchPattern, searchOption).Select(oo =>
+
+        if (theLimit is not null)
         {
-            var itemString = fileNameOnly ? oo.Name : oo.FullName;
-            if (parFormat.isNullOrEmpty())
+            var theFF = new findFirst.XFindFirst(theDir, searchPattern);
+            var recursive = searchOption == SearchOption.AllDirectories;
+            var aList = new List<string>();
+            var counter = 0;
+            theFF.Trace(recursive, (string fullPath, WIN32_FIND_DATA findData) =>
+            {
+                aList.Add(fileNameOnly ? findData.cFileName : fullPath);
+                counter++;
+                return counter < theLimit;
+            });
+            return aList;
+        }
+
+        if (theLimit == null)
+        {
+            var theDirInfo = new DirectoryInfo(theDir);
+            var theFiles = theDirInfo.EnumerateFiles(searchPattern, searchOption).Select(oo =>
+            {
+                var itemString = fileNameOnly ? oo.Name : oo.FullName;
+                if (parFormat.isNullOrEmpty())
+                    return itemString;
+
+                itemString = parFormat
+                    .Replace("$item", itemString)
+                    .Replace("$name", oo.Name)
+                    .Replace("$fullName", oo.FullName)
+                    .Replace("$extension", oo.Extension)
+                    .Replace("$directory", oo.DirectoryName ?? "")
+                    .Replace("$size", oo.Length.ToString())
+                    .Replace("$created", oo.CreationTime.ToString("o"))
+                    .Replace("$modified", oo.LastWriteTime.ToString("o"))
+                    .Replace("$accessed", oo.LastAccessTime.ToString("o"))
+                    .Replace(@"\n", Environment.NewLine);
                 return itemString;
+            });
 
-            itemString = parFormat
-                .Replace("$item", itemString)
-                .Replace("$name", oo.Name)
-                .Replace("$fullName", oo.FullName)
-                .Replace("$extension", oo.Extension)
-                .Replace("$directory", oo.DirectoryName ?? "")
-                .Replace("$size", oo.Length.ToString())
-                .Replace("$created", oo.CreationTime.ToString("o"))
-                .Replace("$modified", oo.LastWriteTime.ToString("o"))
-                .Replace("$accessed", oo.LastAccessTime.ToString("o"))
-                .Replace(@"\n", Environment.NewLine);
-            return itemString;
-        });
-
-        var theDirs = theDirInfo.EnumerateDirectories(searchPattern, searchOption).Select(oo => fileNameOnly ? oo.Name : oo.FullName);
-        var theAll = theFiles.Concat(theDirs);
-
-        if (theAll != null && theLimit != null)
-            theAll = theAll.Take(theLimit);
-        return theAll;
+            var theDirs = theDirInfo.EnumerateDirectories(searchPattern, searchOption).Select(oo => fileNameOnly ? oo.Name : oo.FullName);
+            var theAll = theFiles.Concat(theDirs);
+            return theAll;
+        }
+        return default;
     }
 }
